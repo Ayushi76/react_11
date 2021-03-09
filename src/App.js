0dfb380c72542c7c87837d5c09d00a8b7fdcfa47
Ayushi <miss.ayushigaurav01@gmail.com>
2021-03-09 16:32:25 +0530

    my react host

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () =>
+{
+const state = useState();
+const [count, setCount] =  useState(0);
+
+
+const IncNum = () => {
+  setCount(count + 1);
+// count++;
+//console.log("clicked" + count++);
+};
+
+  return(
+    <>
+    <h1>{count}</h1>
+    <button onClick={IncNum}> Click Me </button>
+
+    </>
   );
-}
+};
+
 
 export default App;
